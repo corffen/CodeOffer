6109f32c7d89e7cc2c676629c46a1aca82dd5ec7
corffen <corffen@gmail.com>
2023-06-24 22:29:44 +0800

    坐旋转字符串

--- src/main/java/org/gordon/string/StringSolution.java
+++ src/main/java/org/gordon/string/StringSolution.java
@@ -4,6 +4,7 @@ public class StringSolution {
     /**
      * 剑指 Offer 05. 替换空格
      * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+     *
      * @param s
      * @return
      */
@@ -12,14 +13,14 @@ public class StringSolution {
         for (char c : s.toCharArray()) {
             if (c == ' ') {
                 sb.append("%20");
-            }else {
+            } else {
                 sb.append(c);
             }
         }
         return sb.toString();
     }
 
-    public String replaceSpace2(String s){
+    public String replaceSpace2(String s) {
         int count = 0;
         for (char c : s.toCharArray()) {
             if (c == ' ') {
@@ -30,17 +31,39 @@ public class StringSolution {
             return s;
         }
         char[] newArray = new char[s.length() + count * 2];
-        int j = newArray.length-1;
-        for (int i = s.length()-1;i>=0;i--) {
+        int j = newArray.length - 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
-            if (c==' '){
+            if (c == ' ') {
                 newArray[j--] = '0';
                 newArray[j--] = '2';
                 newArray[j--] = '%';
-            }else{
+            } else {
                 newArray[j--] = c;
             }
         }
         return new String(newArray);
+    }
+
+    /**
+     * 剑指 Offer 58 - II. 左旋转字符串
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+     */
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder sb = new StringBuilder(s);
+        reverseString(sb, 0, n);
+        reverseString(sb, n, s.length()-1);
+        reverseString(sb, 0, s.length()-1);
+        return sb.toString();
+    }
+
+    private void reverseString(StringBuilder sb, int l, int r) {
+        while (l < r) {
+            char temp = sb.charAt(l);
+            sb.setCharAt(l, sb.charAt(r));
+            sb.setCharAt(r, temp);
+            l++;
+            r--;
+        }
     }
 }
